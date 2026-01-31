27bb3c23ca8323c0cd01a6d4418ba2993f484b01
soniiix <quent.couzinet@gmail.com>
2026-01-31 23:18:54 +0100

    style: add blue glow effects in background

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,32 @@ import { ArrowRightIcon, MagnifyingGlassIcon } from "@phosphor-icons/react";
 
 export default function Home() {
     return (
-        <div className="text-3xl text-center font-normal h-screen flex flex-col items-center justify-between pt-46 pb-6 px-6">
-            <div className="flex flex-col items-center gap-3">
+        <div className="text-3xl text-center font-normal h-screen flex flex-col items-center justify-between pt-46 pb-6 px-6 relative overflow-hidden">
+            <div className="flex flex-col items-center gap-3 relative">
+                <div className="top-blue-glow"></div>
                 <img src="/ps-logo.png" alt="PlayStation logo" className="h-8" />
                 <h1 className="font-mont text-4xl sm:text-5xl mb-2">PSN TRACKER</h1>
                 <span className="text-2xl sm:text-3xl font-light">Track and analyze your PlayStation trophy progress.</span>
 
-                <form className="bg-secondary-bg focus-within:outline-2 focus-within:outline-offset-4 focus-within:outline-blue mt-6 pl-4 pr-2 py-2 rounded-normal text-neutral w-full max-w-[530px] text-lg flex flex-row items-center justify-between gap-2">
+                <form className="bg-secondary-bg focus-within:outline-2 focus-within:outline-offset-3 focus-within:outline-blue mt-8 pl-4 pr-1.5 py-1.5 rounded-normal text-neutral w-full max-w-[530px] text-lg flex flex-row items-center justify-between gap-2">
                     <div className="flex items-center gap-3 flex-1">
                         <MagnifyingGlassIcon size={26} className=" mt-0.5"/>
                         <input 
                             type="text" 
                             placeholder="Enter a PSN ID" 
                             className="flex-1 min-w-0 w-full focus:outline-none placeholder:text-neutral text-white"
+                            required
                         />
                     </div>
                     <button 
                         type="submit" 
-                        className="bg-blue-gradient px-5 py-1.5 rounded-xl text-white cursor-pointer shrink-0 w-auto"
+                        className="bg-blue-gradient px-5 py-2 rounded-xl text-white cursor-pointer shrink-0 w-auto"
                     >
                         Search
                     </button>
                 </form>
-                
             </div>
+
             <div className="text-sm flex flex-row items-center justify-center gap-1.5 border px-3 py-1 rounded-[10px] border-neutral">
                 <span className="text-neutral">You're viewing the beta version.</span>
                 <a 
@@ -37,6 +39,7 @@ export default function Home() {
                     Read more
                     <ArrowRightIcon className="mt-0.5" size={14} />
                 </a>
+                <div className="bottom-blue-glow"></div>
             </div>
         </div>
     );
