d04b6a3bf770269d78e908019979d9e077c1ce07
soniiix <quent.couzinet@gmail.com>
2026-04-06 22:16:16 +0200

    feat: add the trophy summary section

--- components/profile/ProfileHeader.tsx
+++ components/profile/ProfileHeader.tsx
@@ -4,9 +4,16 @@ import { trimAboutMe } from "@/lib/utils/trimAboutMe";
 export default async function ProfileHeader({ psnId }: { psnId: string }) {
     const profile = await getProfileFromPsnId(psnId);
 
+    const earnedTrophies = profile.trophySummary?.earnedTrophies;
+    const totalTrophies =
+        (earnedTrophies?.platinum ?? 0) +
+        (earnedTrophies?.gold ?? 0) +
+        (earnedTrophies?.silver ?? 0) +
+        (earnedTrophies?.bronze ?? 0);
+
     return (
         <section className="bg-secondary-bg w-full rounded-normal h-[350px] flex flex-col justify-between items-center border-l-gradient-rounded overflow-hidden">
-            <div className="w-full h-[187px] flex items-center justify-center gap-[164px]">
+            <div className="w-full h-[187px] flex items-center justify-center gap-[164px] pl-3.5">
                 {/* Profile info */}
                 <div className="flex items-center justify-center gap-5">
                     <img 
@@ -54,7 +61,47 @@ export default async function ProfileHeader({ psnId }: { psnId: string }) {
 
             <div className="w-full h-[1px] bg-divider"></div>
 
-            <div className="w-full h-[162px] text-center">TODO</div>
+            {/* Trophy summary */}
+            <div className="flex items-center justify-center w-full h-[162px] text-center pl-3.5">
+                <div className="flex items-center justify-between w-[778px] h-full">
+                    {/* Platinum trophies */}
+                    <div className="flex items-end mb-2 justify-start gap-2">
+                        <div className="w-14.5 h-17 flex items-end justify-center shrink-0">
+                            <img src="/images/platinum-trophy.png" alt="Platinum trophy icon" className="block w-full h-full object-contain mb-0.25" />
+                        </div>
+                        <span className="text-[22px] font-medium leading-none">{earnedTrophies?.platinum ?? 0}</span>
+                    </div>
+                    {/* Gold trophies */}
+                    <div className="flex items-end justify-center gap-2">
+                        <div className="w-14.5 h-14.5 flex items-end justify-center shrink-0">
+                            <img src="/images/gold-trophy.png" alt="Gold trophy icon" className="block w-full h-full object-contain" />
+                        </div>
+                        <span className="text-[22px] font-medium leading-none">{earnedTrophies?.gold ?? 0}</span>
+                    </div>
+                    {/* Silver trophies */}
+                    <div className="flex items-end justify-center gap-2">
+                        <div className="w-14.5 h-14.5 flex items-end justify-center shrink-0">
+                            <img src="/images/silver-trophy.png" alt="Silver trophy icon" className="block w-full h-full object-contain" />
+                        </div>
+                        <span className="text-[22px] font-medium leading-none">{earnedTrophies?.silver ?? 0}</span>
+                    </div>
+                    {/* Bronze trophies */}
+                    <div className="flex items-end justify-center gap-2">
+                        <div className="w-14.5 h-14.5 flex items-end justify-center shrink-0">
+                            <img src="/images/bronze-trophy.png" alt="Bronze trophy icon" className="block w-full h-full object-contain" />
+                        </div>
+                        <span className="text-[22px] font-medium leading-none">{earnedTrophies?.bronze ?? 0}</span>
+                    </div>
+                    
+                    <div className="h-17.5 w-[1px] bg-divider"></div>
+
+                    {/* Total trophies */}
+                    <div className="flex flex-col items-start justify-end gap-2.5">
+                        <span className="text-neutral text-[22px] leading-none">Total</span>
+                        <span className="text-[30px] font-medium leading-none">{totalTrophies}</span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
