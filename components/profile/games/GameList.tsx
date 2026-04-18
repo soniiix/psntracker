4c65c85ef4e3b64673044b47df9db5116925239e
soniiix <quent.couzinet@gmail.com>
2026-04-18 12:21:01 +0200

    fix: show only the number of games displayed

--- components/profile/games/GameList.tsx
+++ components/profile/games/GameList.tsx
@@ -86,7 +86,7 @@ export default function GameList({
         <section className="bg-secondary-bg w-full rounded-normal flex flex-col overflow-hidden">
             {/* SECTION HEADER: total games displayed, and filters buttons */}
             <div className="bg-alternate-bg rounded-t-normal flex items-center justify-between px-6 py-3.5">
-                <h2 className="text-[20px]">{games.length}/{totalGamesCount} games displayed</h2>
+                <h2 className="text-[20px]">{games.length} games displayed</h2>
                 <div className="flex items-center gap-3">
                     <button className="px-4 py-1.5 text-lg bg-[#3E4555] text-white rounded-xl flex items-center justify-center gap-2 cursor-pointer">
                         <ArrowsDownUpIcon size={20} className="-mb-0.5 -ml-0.5" />
