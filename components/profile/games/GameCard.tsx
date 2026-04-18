b14d19716b117d9a84b1f7561d3756de9f1562f4
soniiix <quent.couzinet@gmail.com>
2026-04-18 10:57:19 +0200

    feat: add trophy display and progress bar in GameCard component

--- components/profile/games/GameCard.tsx
+++ components/profile/games/GameCard.tsx
@@ -1,6 +1,8 @@
-import { CalendarBlankIcon, ClockIcon } from "@phosphor-icons/react";
+import { CalendarBlankIcon, CheckIcon, ClockIcon } from "@phosphor-icons/react";
 
 export default function GameCard({ game }: { game: any }) {
+    const hasPlatinum = (game?.definedTrophies?.platinum ?? game?.earnedTrophies?.platinum ?? 0) > 0;
+
     console.log(game);
     return (
         <div className="flex w-full">
@@ -11,7 +13,7 @@ export default function GameCard({ game }: { game: any }) {
                 />
             </div>
 
-            <div className="flex justify-between px-5 flex-1">
+            <div className="flex justify-between px-5 py-4 flex-1">
                 {/* GAME INFO */}
                 <div className="flex items-center justify-between">
                     <div className="flex flex-col gap-1">
@@ -26,7 +28,7 @@ export default function GameCard({ game }: { game: any }) {
                             </div>
                         </div>
                         <div className="flex items-center gap-1.5 text-neutral">
-                            <CalendarBlankIcon size={19} className="-mb-0.5"/>
+                            <CalendarBlankIcon size={19} className="-mb-0.5" />
                             <span className="text-[17px]">
                                 Last played on {new Date(game.lastUpdatedDateTime).toLocaleDateString()}
                             </span>
@@ -35,10 +37,73 @@ export default function GameCard({ game }: { game: any }) {
                 </div>
 
                 {/* TROPHIES */}
-                <div className="flex items-center gap-6">
-                    fdz
+                <div className="flex flex-col h-full w-80 justify-center gap-5.5">
+                    {/* TROPHIES EARNED / TOTAL */}
+                    <div className="flex items-center justify-between gap-2">
+                        {/* Platinum */}
+                        <div className="flex items-end justify-start gap-2">
+                            {(hasPlatinum) ? (
+                            <div
+                                className={`h-10 mb-0.5 flex items-end justify-center shrink-0 ${game?.earnedTrophies?.platinum > 0 ? "opacity-100" : "opacity-10"}`}
+                            >
+                                <img
+                                    src="/images/platinum-trophy.png"
+                                    alt="Platinum trophy icon"
+                                    className="block w-full h-full object-contain"
+                                />
+                            </div>
+                            ) : (
+                                <div className="bg-emerald-500/10  p-1.5 rounded-full -mr-1.5 ">
+                                    <CheckIcon size={24} weight="bold" className="text-green-400" />
+                                </div>
+                            )}
+                        </div>
+                        {/* Gold */}
+                        <div className="flex items-end justify-center gap-2">
+                            <div className="w-8 h-9 flex items-end justify-center shrink-0">
+                                <img src="/images/gold-trophy.png" alt="Gold trophy icon" className="block w-full h-full object-contain" />
+                            </div>
+                            <div className="leading-none -mb-1">
+                                <span className="font-medium text-lg">
+                                    {game.earnedTrophies?.gold ?? 0}
+                                </span>
+                                <span className="text-neutral text-[15px]">/{game.definedTrophies?.gold ?? 0}</span>
+                            </div>
+                        </div>
+                        {/* Silver */}
+                        <div className="flex items-end justify-center gap-2">
+                            <div className="w-8 h-9 flex items-end justify-center shrink-0">
+                                <img src="/images/silver-trophy.png" alt="Silver trophy icon" className="block w-full h-full object-contain" />
+                            </div>
+                            <div className="leading-none -mb-1">
+                                <span className="font-medium text-lg">
+                                    {game.earnedTrophies?.silver ?? 0}
+                                </span>
+                                <span className="text-neutral text-[15px]">/{game.definedTrophies?.silver ?? 0}</span>
+                            </div>
+                        </div>
+                        {/* Bronze */}
+                        <div className="flex items-end justify-center gap-2">
+                            <div className="w-8 h-9 flex items-end justify-center shrink-0">
+                                <img src="/images/bronze-trophy.png" alt="Bronze trophy icon" className="block w-full h-full object-contain" />
+                            </div>
+                            <div className="leading-none -mb-1">
+                                <span className="font-medium text-lg">
+                                    {game.earnedTrophies?.bronze ?? 0}
+                                </span>
+                                <span className="text-neutral text-[15px]">/{game.definedTrophies?.bronze ?? 0}</span>
+                            </div>
+                        </div>
+                    </div>
+                    {/* PROGRESS BAR */}
+                    <div className="flex items-center gap-3">
+                        <div className="bg-progress-bar w-full h-3 rounded-full overflow-hidden">
+                            <div className="bg-blue-gradient h-full rounded-full" style={{ width: `${game.progress}%` }} />
+                        </div>
+                        <span className="leading-none text-[17px] text-neutral">{game.progress}%</span>
+                    </div>
                 </div>
-            </div>   
+            </div>
         </div>
     );
 }
