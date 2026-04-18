8a7f039ba01439afdee909aef290a4e3ba28ed0c
soniiix <quent.couzinet@gmail.com>
2026-04-18 12:31:39 +0200

    fix: update stats labels for singular/plural

--- components/profile/ProfileStats.tsx
+++ components/profile/ProfileStats.tsx
@@ -5,11 +5,15 @@ export default function ProfileStats({ stats }: { stats: ProfileStats }) {
         <section className="bg-secondary-bg w-full rounded-normal flex items-center justify-between px-10 py-8">
             <div className="flex items-end justify-center gap-2">
                 <span className="text-2xl font-medium">{stats.gamesPlayed}</span>
-                <span className="text-neutral uppercase text-[15px] font-medium">Games played</span>
+                <span className="text-neutral uppercase text-[15px] font-medium">
+                    {stats.gamesPlayed === 1 ? "Game played" : "Games played"}
+                </span>
             </div>
             <div className="flex items-end justify-center gap-2">
                 <span className="text-2xl font-medium">{stats.completedGames}</span>
-                <span className="text-neutral uppercase text-[15px] font-medium">Completed games</span>
+                <span className="text-neutral uppercase text-[15px] font-medium">
+                    {stats.completedGames === 1 ? "Completed game" : "Completed games"}
+                </span>
             </div>
             <div className="flex items-end justify-center gap-2">
                 <span className="text-2xl font-medium">{stats.completionRate.toFixed(2)}%</span>
